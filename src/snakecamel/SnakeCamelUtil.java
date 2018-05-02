0d73ca897f1349f0f97e9d7dace04251efdaa355
cytokuromu <g16901am@tsuda.ac.jp>
2018-05-02 11:11:35 +0900

    JUnitでテストケースを作成し、プログラムを修正した

--- src/snakecamel/SnakeCamelUtil.java
+++ src/snakecamel/SnakeCamelUtil.java
@@ -5,7 +5,7 @@ public class SnakeCamelUtil {
 	public static String snakeToCamelcase(String snake_case) {
 		String[] words = snake_case.split("_");
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < words.length - 1; i++) {
+		for (int i = 0; i < words.length ; i++) {
 			sb.append(capitalize(words[i]));
 		}
 		return new String(sb);
@@ -14,18 +14,24 @@ public class SnakeCamelUtil {
 	public static String camelToSnakecase(String camelcase) {
 		StringBuilder sb = new StringBuilder();
 		int j = 0;
-		for (int i = 0; i < camelcase.length(); i++) {
-			char c = camelcase.charAt(i);
+		for (int i = 1; i <= camelcase.length(); i++) {
+			char c = camelcase.charAt(j);
 			if (Character.isUpperCase(c)) {
-				sb.append(camelcase.substring(j, i));
 				if (sb.length() > 0) {
 					sb.append("_");
 				}
-				sb.append(Character.toLowerCase(c));
-				j = i;
+				sb.append(uncapitalize(camelcase.substring(j, i)));
+				/*
+				 * if (sb.length() > 0) { sb.append("_"); }
+				 */
+			} else {
+				sb.append(c);
+				// j = i;
 			}
+			j = i;
 		}
-		sb.append(camelcase.substring(j));
+		// sb.append(camelcase.substring(j));
+		// System.out.println(camelcase.substring(j));
 		return new String(sb);
 	}
 	
