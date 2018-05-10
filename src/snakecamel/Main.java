f262c73ab69cff578ee63e6e0b5eed8b3aca9106
cytokuromu <g16901am@tsuda.ac.jp>
2018-05-10 14:24:01 +0900

    空文字の時も対応できるようにSnakeCamelUtilを変更

--- src/snakecamel/Main.java
+++ src/snakecamel/Main.java
@@ -4,9 +4,10 @@ public class Main {
  public static void main(String[] args){
 	SnakeCamelUtil scu=new SnakeCamelUtil();
 	String s="";
-	scu.capitalize(s);
-	scu.uncapitalize(s);
-	scu.snakeToCamelcase(s);
-	scu.camelToSnakecase(s);
+	String re="";
+	re=SnakeCamelUtil.capitalize(s);
+	re=scu.uncapitalize(s);
+	re=scu.snakeToCamelcase(s);
+	re=scu.camelToSnakecase(s);
  }
 }
